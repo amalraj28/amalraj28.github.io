ae89e5ac293ff54a72f4e8d5dfa82591498174ae
Amal Raj <amalraj2701@gmail.com>
2024-02-12 06:52:15 +0530

    Functiona;ities done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,24 @@ const initState = {
 };
 
 const reducer = (currState, action) => {
-	const text = currState.text;
+	const operatorCode = (val) => {
+		if (
+			currState.text &&
+			isNaN(parseInt(currState.text.toString()[currState.text.toString().length - 1]))
+		) {
+			return currState;
+		}
+		const newText = currState.equalPressed
+			? currState.text + val
+			: currState.upperText + currState.text + val;
+		return {
+			...currState,
+			upperText: newText,
+			clearLower: true,
+			decimal: 0,
+			equalPressed: false,
+		};
+	};
 	switch (action.val) {
 		case "1":
 		case "2":
@@ -27,58 +44,68 @@ const reducer = (currState, action) => {
 			return {
 				...currState,
 				text:
-					text === "0" || currState.clearLower
+					currState.text === "0" || currState.clearLower
 						? "" + action.val
-						: text + action.val,
+						: currState.text + action.val,
+				upperText: currState.equalPressed ? "" : currState.upperText,
 				clearLower: false,
+				equalPressed: false
 			};
 
 		case "=":
 			return {
 				...currState,
-				upperText: currState.upperText + currState.text + "=",
-				text: eval(currState.upperText + currState.text),
+				upperText:
+					currState.upperText && !currState.equalPressed
+						? currState.upperText + currState.text + "="
+						: currState.upperText,
+				text: !currState.equalPressed
+					? eval(currState.upperText + currState.text)
+					: currState.text,
 				clearLower: true,
 				equalPressed: true,
 				decimal: 0,
 			};
 
-		case "+": {
-			const newText = currState.text + "+";
-			return { ...currState, upperText: newText, clearLower: true, decimal: 0 };
-		}
-		case "-": {
-			const newText = currState.text + "-";
-			return { ...currState, upperText: newText, clearLower: true, decimal: 0 };
-		}
-		case "*": {
-			const newText = currState.text + "*";
-			return { ...currState, upperText: newText, clearLower: true, decimal: 0 };
-		}
-		case "/": {
-			const newText = currState.text + "/";
-			return { ...currState, upperText: newText, clearLower: true, decimal: 0 };
-		}
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+			return operatorCode(action.val);
 
-		case ".":
-			return currState.decimal === 0
-				? { ...currState, text: text + action.val, decimal: 1 }
-				: currState;
+		case ".": {
+			if (currState.equalPressed) return { ...initState, text: "0." }
+			return currState.clearLower
+				? {
+					...currState,
+					text: "0.",
+					clearLower: false,
+					equalPressed: false,
+					decimal: 1,
+				}
+				: currState.decimal === 0
+					? { ...currState, text: currState.text + action.val, decimal: 1 }
+					: currState;
+		}
 
 		case "CE":
 			return initState;
 
-		case "bksp":
-			return text.length < 2 || (text.length === 2 && text[0] === "-")
-				? currState
-				: text[text.length - 1] === "."
-				? { ...currState, text: text.slice(0, -1), decimal: 0 }
-				: { ...currState, text: text.slice(0, -1) };
-
+		case "bksp": {
+			return currState.equalPressed
+				? initState
+				: currState.text.length < 2 ||
+					(currState.text.length === 2 && currState.text[0] === "-")
+					? { ...currState, text: "0" }
+					: currState.text[currState.text.length - 1] === "."
+						? { ...currState, text: currState.text.slice(0, -1), decimal: 0 }
+						: { ...currState, text: currState.text.slice(0, -1) };
+		}
 		case "plus-minus": {
-			const num = parseInt(text);
+			const num = parseInt(currState.text);
 			if (num === 0) return currState;
-			const newText = Math.sign(num) === 1 ? `-${text}` : text.slice(1);
+			const newText =
+				Math.sign(num) === 1 ? `-${currState.text}` : currState.text.slice(1);
 
 			return { ...currState, text: newText };
 		}
